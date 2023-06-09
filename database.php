e32d1140c82d983c5bc769be1e5492eb1d097836
spiekarski18 <122455083+spiekarski18@users.noreply.github.com>
2023-06-09 19:40:32 +0200

    Update database.php

--- database.php
+++ database.php
@@ -3,9 +3,8 @@
 $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
-$dbName = "nazwaBazyDanych";
+$dbName = "ApexDB";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Ups! Coś nie wyszło...;");
 }
-
